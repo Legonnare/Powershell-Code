d90c080f5714599436adbef8c8a799620fffdd50
Legonnare <legonnare@gmail.com>
2023-02-28 10:58:10 -0600

    Create get-nwsalerts.ps1

---
+++ NWS Alerts/get-nwsalerts.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding()]
+param (
+  $State = "MN",
+  $County = "Hennepin"
+)
+Add-Type -AssemblyName PresentationFramework
+
+$ZoneIDs = Import-CSV -Path 'C:\Users\legon\OneDrive\Documents\GitHub\Powershell-Code\NWS Alerts\all-geocodes-v2019.csv'
+
+$CountyID = $ZoneIDs | Where-Object -Property Name -Like "$($County) County"
+
+$AlertURI = "https://api.weather.gov/alerts/active?zone=$($State)C0$($CountyID.CountyCode)"
+
+$id = ""
+
+do{
+  Write-Verbose "++++++++++++++++++++++++  Starting script run at $(get-date)  ++++++++++++++++++++++++"
+  $NWSalerts = @()
+  $NWSalerts = Invoke-RestMethod -Uri $AlertURI
+
+  If (($NWSalerts.features.count -gt 0) -and ($NWSalerts.features.id -ne $id)){
+    $AlertArray = @()
+    Foreach ($Alert in $NWSalerts.features){
+      If ($Alert.properties.areaDesc -like "*$($County)*"){
+        $Array = New-Object -TypeName pscustomobject
+        $Array | Add-Member -MemberType NoteProperty -Name "Location" -Value $Alert.properties.areaDesc
+        $Array | Add-Member -MemberType NoteProperty -Name "Sent" -Value $Alert.properties.Sent
+        $Array | Add-Member -MemberType NoteProperty -Name "Effective" -Value $Alert.properties.Effective
+        $Array | Add-Member -MemberType NoteProperty -Name "Ends" -Value $Alert.properties.Ends
+        $Array | Add-Member -MemberType NoteProperty -Name "Certainty" -Value $Alert.properties.Certainty
+        $Array | Add-Member -MemberType NoteProperty -Name "Severity" -Value $Alert.properties.severity
+        $Array | Add-Member -MemberType NoteProperty -Name "Headline" -Value $Alert.properties.Headline
+        $Array | Add-Member -MemberType NoteProperty -Name "Description" -Value $Alert.properties.Description
+        $Array | Add-Member -MemberType NoteProperty -Name "Instructions" -Value $Alert.properties.Instruction
+        $Array | Add-Member -MemberType NoteProperty -Name "Reported By" -Value $Alert.properties.senderName
+      }
+      $AlertArray += $Array
+    }
+    $id = $NWSalerts.features.id
+    $Message = $AlertArray | Out-String
+    [System.Windows.MessageBox]::Show("$Message `n `n Acknowledge?",'+++++  NWS ALERT +++++','OK','Warning')
+  }
+  Start-Sleep -Seconds 600
+} Until ($NWSalerts.count -eq 0)
